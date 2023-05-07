1258348f8d05f4012ad1b3b10c14e64c4916a109
Dhaval Patel <developer.dahvalp@gmail.com>
2023-05-07 12:41:17 +0530

    add small react app

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.css';
+import App from "./App";
+import "./index.css";
 
-let curDate = new Date();
-curDate = curDate.getHours();
-let greeting = "";
-const cssStyle = {};
-
-if (curDate >= 1 && curDate < 12) {
-  greeting = "Good morning";
-  cssStyle.color = "green";
-} else if (curDate >= 12 && curDate < 19) {
-  greeting = "Good afternoon";
-  cssStyle.color = "orange";
-} else {
-  greeting = "Good night";
-  cssStyle.color = "black";
-}
-
-ReactDOM.render(
-  <>
-    <div>
-      <h1>
-        Hello Sir, <span style={cssStyle}> {greeting} </span>
-      </h1>
-    </div>
-  </>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
